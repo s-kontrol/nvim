13dcc53b9fefcbc1e6033de012f43ff77cdcdbd6
s-kontrol <servicios.mruiz@gmail.com>
2026-02-27 15:19:01 -0300

    rebase fix

--- init.lua
+++ init.lua
@@ -649,6 +649,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- You can add other tools here that you want Mason to install
+        'stylua', -- Used to format Lua code
+        'ansiblels', -- ansible
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
